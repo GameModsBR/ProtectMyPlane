88dcda47a425f9d2e5ce5a553c2f4b63da136233
joserobjr <jose.rob.jr@gmail.com>
2015-12-27 15:11:37 -0300

    Fixes error when other mod attempt to add dependency on this mod

--- src/main/java/br/com/gamemods/protectmyplane/ProtectMyPlaneMod.java
+++ src/main/java/br/com/gamemods/protectmyplane/ProtectMyPlaneMod.java
@@ -1,6 +1,8 @@
 package br.com.gamemods.protectmyplane;
 
+import com.google.common.eventbus.EventBus;
 import cpw.mods.fml.common.DummyModContainer;
+import cpw.mods.fml.common.LoadController;
 import cpw.mods.fml.common.ModMetadata;
 
 public class ProtectMyPlaneMod extends DummyModContainer
@@ -13,5 +15,11 @@ public class ProtectMyPlaneMod extends DummyModContainer
         metadata.name="Protect My Plane";
         metadata.modId="ProtectMyPlane";
         metadata.version="1.0";
+    }
+
+    @Override
+    public boolean registerBus(EventBus bus, LoadController controller)
+    {
+        return true;
     }
 }
